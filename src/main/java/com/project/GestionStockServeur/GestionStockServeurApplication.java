6b5c1aa0b67654a7530bd93bb6a82d106b26080b
dhekra <dhekra.benhassine.1992@gmail.com>
2023-06-19 14:39:49 +0100

    first commit

---
+++ src/main/java/com/project/GestionStockServeur/GestionStockServeurApplication.java
@@ -0,0 +1,13 @@
+package com.project.GestionStockServeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockServeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/GestionStockServeur/GestionStockServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GestionStockServeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
